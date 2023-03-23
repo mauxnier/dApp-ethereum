0feb58977bf45554a66cdd5c7b39ac8dae4d9797
Killian Monnier <mauxnier@gmail.com>
2023-03-23 15:57:45 +0100

    ajout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { Greeter } from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'; // l'adresse du contrat déployé
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
   );
 }
 
