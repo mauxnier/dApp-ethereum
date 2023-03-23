62d9451c2c10c0dd0383995cbf477d9b71917562
Killian Monnier <mauxnier@gmail.com>
2023-03-23 16:47:50 +0100

    ajout

--- README.md
+++ README.md
@@ -8,15 +8,26 @@
 - ethers (alternative à web3.js) : permet d'intéragir avec la blockchain Ethereum.
 
 ## Lancement
-### Compilation des changements
+
+Dans la react-app, il faut lancer la commande suivante pour lancer le serveur de développement :
+
+### Compilation des changements des contrats (fichiers .sol dans le dossier contracts)
+
 npx hardhat compile
 
 ### Création des noeuds locaux (on doit avoir 2 terminaux ouverts)
+
 npx hardhat node
 
 ### Déploiement du contrat (on a besoin des noeuds locaux)
+
 npx hardhat run scripts/deploy.js --network localhost
 
 ## MetaMask (utiliser l'extension navigateur)
+
 Créer un compte et changer le réseau en "localhost 8545"
 Faire "Import Account" et importer un des comptes qui a été créé dans le terminal (celui qui a lancé les noeuds locaux) avec sa clé privée.
+
+## Lancer le serveur de développement
+
+npm start
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,58 @@
-import './App.css';
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { Greeter } from './artifacts/contracts/Greeter.sol/Greeter.json';
+import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 
-const greeterAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'; // l'adresse du contrat déployé
+const greeterAddress = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'; // l'adresse du contrat déployé
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
+
+  // Fonction qui permet de se connecter à MetaMask
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // Fonction qui permet de récupérer le message du contrat
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+        setGreetingValue(data);
+      } catch (err) {
+        console.log('Error: ', err);
+      }
+    }
+  }
+
+  // Fonction qui permet de mettre à jour le message du contrat
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue('');
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
-   
+    <div className="App">
+      <p>{greeting}</p>
+      <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+      <button onClick={setGreeting}>Set Greeting</button>
+    </div>
   );
 }
 
