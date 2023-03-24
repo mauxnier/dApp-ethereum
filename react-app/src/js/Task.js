bb032812c016b2d0b4933c44e12093c7f4fb8744
Killian Monnier <mauxnier@gmail.com>
2023-03-24 12:36:26 +0100

    syntax

--- react-app/src/js/Task.js
+++ react-app/src/js/Task.js
@@ -1,16 +1,16 @@
-import { List , ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import '../css/Task.css';
 
-const Task=({taskText, onClick})=>{
+const Task = ({ taskText, onClick }) => {
     return (
-        <List className="todo__list"> 
+        <List className="todo__list">
             <ListItem>
                 <ListItemAvatar />
-                    <ListItemText primary={taskText} />
+                <ListItemText primary={taskText} />
             </ListItem>
-            <DeleteIcon fontSize="large" style={{opacity:0.7}} onClick={onClick}/>
-        </List> 
+            <DeleteIcon fontSize="large" style={{ opacity: 0.7 }} onClick={onClick} />
+        </List>
     )
 };
 
